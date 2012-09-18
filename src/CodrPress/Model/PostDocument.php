2a284503470d25e336c033b817f7ace97b886eb8
David Henning <madcat.me@gmail.com>
2012-09-18 23:12:56 +0200

    use url generator

--- src/CodrPress/Model/PostDocument.php
+++ src/CodrPress/Model/PostDocument.php
@@ -13,4 +13,15 @@ class PostDocument extends Document {
         $this->setCollectionName('posts');
     }
 
+    public function getLink() {
+        $timestamp = strtotime($this->getProperty('created'));
+        $params = array(
+            'year' => date('Y', $timestamp),
+            'month' => date('m', $timestamp),
+            'day' => date('d', $timestamp),
+            'slug' => $this->getProperty('slug')
+        );
+
+        return $this->_oApp['url_generator']->generate('post', $params);
+    }
 }
