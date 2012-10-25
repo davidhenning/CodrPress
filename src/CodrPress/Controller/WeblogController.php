48005bb7848041b61d0c565b4d7631ebd780dd76
David Henning <madcat.me@gmail.com>
2012-10-25 20:18:30 +0200

    renaming

--- src/CodrPress/Controller/WeblogController.php
+++ src/CodrPress/Controller/WeblogController.php
@@ -14,20 +14,20 @@ class WeblogController implements ControllerProviderInterface {
 
     public function connect(Application $app) {
         $router = $app['controllers_factory'];
-        $postList = new PostCollection($app);
-        $postList->sortBy('created_at', 'desc');
-        $pageList = new PostCollection($app);
-        $pageList->sortBy('created_at', 'desc');
+        $postCollection = new PostCollection($app);
+        $postCollection->sortBy('created_at', 'desc');
+        $pageCollection = new PostCollection($app);
+        $pageCollection->sortBy('created_at', 'desc');
 
-        $router->get('/', function() use($app, $postList, $pageList) {
+        $router->get('/', function() use($app, $postCollection, $pageCollection) {
             return $app['twig']->render('posts.twig', array(
-                'posts' => $postList->findPosts(),
-                'pages' => $pageList->findPages()
+                'posts' => $postCollection->findPosts(),
+                'pages' => $pageCollection->findPages()
             ));
         })->bind('home');
 
-        $router->get('/{year}/{month}/{day}/{slug}/', function($year, $month, $day, $slug) use($app, $postList, $pageList) {
-            $posts = $postList->findBySlug($slug);
+        $router->get('/{year}/{month}/{day}/{slug}/', function($year, $month, $day, $slug) use($app, $postCollection, $pageCollection) {
+            $posts = $postCollection->findBySlug($slug);
 
             if(count($posts) === 0) {
                 throw new PostNotFoundException("The url '{$app['request']->getUri()}' does not exist!");
@@ -35,7 +35,7 @@ class WeblogController implements ControllerProviderInterface {
 
             return $app['twig']->render('post.twig', array(
                 'posts' => $posts,
-                'pages' => $pageList->findPages()
+                'pages' => $pageCollection->findPages()
             ));
         })
         ->assert('year', '\d{4}')
