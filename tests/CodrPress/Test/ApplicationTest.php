7c198772da18c6421af7bd898f3daa1b2c82b21d
David Henning <madcat.me@gmail.com>
2012-10-25 22:41:15 +0200

    test for tags

--- tests/CodrPress/Test/ApplicationTest.php
+++ tests/CodrPress/Test/ApplicationTest.php
@@ -19,6 +19,7 @@ class ApplicationTest extends WebTestCase {
         // add test post
         $post = new Post($app);
         $post->setProperty('slugs', array('slug'));
+        $post->setProperty('tags', array('Test'));
         $post->setProperty('title', 'test');
         $post->setProperty('body', 'test');
         $post->setProperty('status', 'published');
@@ -33,8 +34,16 @@ class ApplicationTest extends WebTestCase {
         $client->request('GET', date('/Y/m/d') . '/slug/');
         $this->assertTrue($client->getResponse()->isOk());
 
+        // existing tag
+        $client->request('GET', '/tag/Test/');
+        $this->assertTrue($client->getResponse()->isOk());
+
         // post fail
         $client->request('GET', '/20122/09/132/slug/');
+        $this->assertFalse($client->getResponse()->isOk());
+
+        // tag fail
+        $client->request('GET', '/tag/MUHAHA/');
         $this->assertFalse($client->getResponse()->isOk());
 
         // post fail
