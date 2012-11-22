8285dcb5c551b603cd67955cc44f21f34948bdf8
David Henning <madcat.me@gmail.com>
2012-11-22 20:17:55 +0100

    code style

--- src/CodrPress/ViewHelper/PostRestViewHelper.php
+++ src/CodrPress/ViewHelper/PostRestViewHelper.php
@@ -102,8 +102,6 @@ class PostRestViewHelper
 
     public function getPostDeleteOutput(Application $app, $id)
     {
-
-
         try {
             $post = new Post($app);
             $post->load($id);
