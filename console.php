aa6bb740f7dcb8b74468f6778e6daee853a53030
David Henning <madcat.me@gmail.com>
2013-04-02 18:50:45 +0200

    check if config file exists to prevent errors if it's not there, added config command to create or edit the config file

--- console.php
+++ console.php
@@ -2,13 +2,17 @@
 
 require realpath('vendor/autoload.php');
 
+$console = new Symfony\Component\Console\Application();
+
 $config = new CodrPress\Config();
 $config->setBaseDir(realpath(__DIR__));
 $configFile = (isset($overrideConfigFile)) ? $config->getConfDir() . "/{$overrideConfigFile}" : $config->getConfDir() . '/codrpress.yml';
-$config->addConfigFile($configFile);
 
-$app = new CodrPress\Application($config);
-$console = new Symfony\Component\Console\Application();
-$console->add(new CodrPress\Console\Command\User\Create($app));
+if (is_readable($configFile)) {
+    $config->addConfigFile($configFile);
+    $app = new CodrPress\Application($config);
+    $console->add(new CodrPress\Console\Command\User\Create($app));
+}
 
+$console->add(new \CodrPress\Console\Command\Config\Create());
 $console->run();
