fbd619fe3dbd573cad044391caaaa9b9b6bd93a7
David Henning <madcat.me@gmail.com>
2013-04-02 16:09:31 +0200

    added console commands

---
+++ console.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = new CodrPress\Config();
+$config->setBaseDir(realpath(__DIR__));
+$configFile = (isset($overrideConfigFile)) ? $config->getConfDir() . "/{$overrideConfigFile}" : $config->getConfDir() . '/codrpress.yml';
+$config->addConfigFile($configFile);
+
+$app = new CodrPress\Application($config);
+$console = new Symfony\Component\Console\Application();
+$app->add(new CodrPress\Console\Command\User\Create($app));
+
+$console->run();
